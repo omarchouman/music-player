088bcb387fa2570798dd6a9c07d387e5e3fbf3c1
Omar Chouman <omar.chouman@hotmail.com>
2021-01-16 16:15:44 +0200

    Music Player

---
+++ precache-manifest.4664d3f95463973aa3bd358fa145954d.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c31c71176f243370a9d0",
+    "url": "/css/app.2941da79.css"
+  },
+  {
+    "revision": "cd3432d4b4e46fe054422763405dfd0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c31c71176f243370a9d0",
+    "url": "/js/app.352ac42b.js"
+  },
+  {
+    "revision": "160fae7cbe873d87c6e9",
+    "url": "/js/chunk-vendors.49d41e03.js"
+  },
+  {
+    "revision": "3692cfc64231ba3f27721ceaedb4c313",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "ed20c03c5373ae530183d62776b18b49",
+    "url": "/media/Air.ed20c03c.mp3"
+  },
+  {
+    "revision": "2cf40801b1b8dbae95f2988185af9edd",
+    "url": "/media/Corporate-Motivational-Ambient.2cf40801.mp3"
+  },
+  {
+    "revision": "6183713f4d9cc9031170f6e801d5107b",
+    "url": "/media/Deal.6183713f.mp3"
+  },
+  {
+    "revision": "0856cba20fbb2b4c507f29301f44212e",
+    "url": "/media/Deep-Relaxation.0856cba2.mp3"
+  },
+  {
+    "revision": "3803613dc5ae724352d76c74ad423736",
+    "url": "/media/Dramatic-Emotional.3803613d.mp3"
+  },
+  {
+    "revision": "9dee71ea8209677ee669b72b2eed7a3a",
+    "url": "/media/Extreme.9dee71ea.mp3"
+  },
+  {
+    "revision": "2e9e67775b4bdc0972c539b210cd6e62",
+    "url": "/media/Inspirational-Moment.2e9e6777.mp3"
+  },
+  {
+    "revision": "164aa85459168b6626ae83ed6e91a7c0",
+    "url": "/media/Inspiring-Acoustic.164aa854.mp3"
+  },
+  {
+    "revision": "063db74a7161c3c84133807e8525ef50",
+    "url": "/media/Modern-Egypt.063db74a.mp3"
+  },
+  {
+    "revision": "37adb0dd628803602de44c055fce5e86",
+    "url": "/media/Mountain.37adb0dd.mp3"
+  },
+  {
+    "revision": "08c492a990788bd2289ece8fecdeb502",
+    "url": "/media/Purpose.08c492a9.mp3"
+  },
+  {
+    "revision": "e3e758af35a617f1e9d970172aeb99d8",
+    "url": "/media/Stronger.e3e758af.mp3"
+  },
+  {
+    "revision": "20dd3a1b5aa7bc2c38f3b267bad2152f",
+    "url": "/media/Success.20dd3a1b.mp3"
+  },
+  {
+    "revision": "eb171046b2fbed9796fffcc9702f251a",
+    "url": "/media/Vip.eb171046.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
